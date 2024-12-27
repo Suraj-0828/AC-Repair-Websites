65a5a1a0ccfe0e1cc5f8d42a9846c09ee1cef8e3
Suraj Jha <134250774+Suraj-0828@users.noreply.github.com>
2024-12-27 18:11:42 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,205 @@
+<header class="header header-layout1">
+
+    <div class="header-topbar">
+        <div class="container-fluid">
+            <div class="row align-items-center">
+                <div class="col-12">
+                    <div class="d-flex align-items-center justify-content-between">
+                        <ul class="contact__list d-flex flex-wrap align-items-center list-unstyled mb-0">
+
+                            <li>
+                                <button class="miniPopup-emergency-trigger button-black" type="button">24/7 Emergency</button>
+                                <div id="miniPopup-emergency" class="miniPopup miniPopup-emergency text-center">
+                                    <div class="emergency__icon">
+                                        <i class="icon-call3"></i>
+                                    </div>
+                                    <a href="tel:+416-857-4459" class="phone__number">
+                                        <i class="icon-phone"></i> <span>+416-857-4459</span>
+                                    </a>
+                                    <p>Get in touch with us for all your HVAC needs—we're here to help you stay comfortable year-round!
+                                    </p>
+                                    <a href="contact-us.php" class="btn btn__secondary btn__link btn__block">
+                                        <span>Book Now</span> <i class="icon-arrow-right"></i>
+                                    </a>
+                                </div><!-- /.miniPopup-emergency -->
+                            </li>
+
+                            <div class="pr-3">
+                                <i class="icon-phone"></i> <a class="aabbbaa:hover text-black footer-imp" href="tel:+416-857-4459">+416-857-4459</a>
+                            </div>
+                            <div>
+                                <i class="icon-location"></i> <a class="aabbbaa:hover text-black footer-imp" href="javascript:void(0)">One HVAC System,
+                                    Toronto, ON, Canada.</a>
+                            </div>
+
+                        </ul>
+
+
+                        <div class="d-flex">
+                            <ul class="social-icons list-unstyled mb-0 mr-30">
+                                <li><a href="#"><img src="assets/images/social-media/facebook.png" alt=""></a></li>
+                                <li><a href="#"><img src="assets/images/social-media/instagram.png" alt=""></a></li>
+                                <li><a href="#"><img src="assets/images/social-media/twitter.png" alt=""></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <nav class="navbar navbar-expand-lg sticky-navbar bg-black">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">
+                <img class="logo-fix" src="assets/images/home/logo.png" class="logo-light" alt="logo">
+            </a>
+            <button class="navbar-toggler" type="button">
+                <span class="menu-lines"><span></span></span>
+            </button>
+            <div class="collapse navbar-collapse" id="mainNavigation">
+                <ul class="navbar-nav ml-auto">
+
+                    <li class="nav__item ">
+                        <a href="index.php" class="nav__item-link color-white">Home</a>
+                    </li>
+
+                    <li class="nav__item has-dropdown bg-black">
+                        <a href="javascript:void(0)" data-toggle="dropdown" class="dropdown-toggle nav__item-link color-white">About Us</a>
+                        <ul class="dropdown-menu bg-black">
+                            <li class="nav__item">
+                                <a href="about-us.php" class="nav__item-link color-white">Company Profile</a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="about-directors.php" class="nav__item-link color-white">About Directors</a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="vision-and-mission.php" class="nav__item-link color-white">Vision and Mission</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li class="nav__item has-dropdown bg-black">
+                        <a href="#" data-toggle="dropdown" class="dropdown-toggle nav__item-link color-white">Furnaces</a>
+                        <ul class="dropdown-menu bg-black">
+                            <li class="nav__item">
+                                <a href="furnace-installation.php" class="nav__item-link color-white">Furnace Installation</a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="furnace-repair.php" class="nav__item-link color-white">Furnace Repair</a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="furnace-maintenance.php" class="nav__item-link color-white">Furnace Maintenance</a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="furnace-buying-guide.php" class="nav__item-link color-white">Furnace Buying Guide</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li class="nav__item has-dropdown bg-black">
+                        <a href="commercial-hvac-guide.php" data-toggle="dropdown" class="dropdown-toggle nav__item-link color-white">Commercial Solutions</a>
+                        <ul class="dropdown-menu bg-black">
+                            <li class="nav__item">
+                                <a href="commercial-hvac-guide.php" class="nav__item-link color-white">Commercial HVAC Guide</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li class="nav__item has-dropdown bg-black">
+                        <a href="#" data-toggle="dropdown" class="dropdown-toggle nav__item-link color-white">Heat Pumps</a>
+                        <ul class="dropdown-menu bg-black">
+                            <li class="nav__item">
+                                <a href="heat-pump-installation.php" class="nav__item-link color-white">Heat Pump Installation</a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="heat-pump-repair.php" class="nav__item-link color-white">Heat Pump Repair</a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="heat-pump-maintenance.php" class="nav__item-link color-white">Heat Pump Maintenance</a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="heat-pump-buying-guide.php" class="nav__item-link color-white">Heat Pump Buying Guide</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li class="nav__item has-dropdown bg-black">
+                        <a href="#" data-toggle="dropdown" class="dropdown-toggle nav__item-link color-white">Water Heaters</a>
+                        <ul class="dropdown-menu bg-black">
+                            <li class="nav__item">
+                                <a href="tankless-water-heaters.php" class="nav__item-link color-white">Tankless Water Heaters</a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="water-heater-installation.php" class="nav__item-link color-white">Water Heater Installation</a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="water-heater-repair.php" class="nav__item-link color-white">Water Heater Repair</a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="water-heater-maintenance.php" class="nav__item-link color-white">Water Heater Maintenance</a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="water-heater-buying-guide.php" class="nav__item-link color-white">Water Heater Guide</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li class="nav__item has-dropdown bg-black">
+                        <a href="#" data-toggle="dropdown" class="dropdown-toggle nav__item-link color-white">Water Purification</a>
+                        <ul class="dropdown-menu bg-black">
+                            <li class="nav__item">
+                                <a href="water-softener-installation.php" class="nav__item-link color-white">Water Softener Installation</a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="reverse-osmosis-installation.php" class="nav__item-link color-white">Reverse Osmosis System Installation</a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="carbon-filter-installation.php" class="nav__item-link color-white">Water Carbon Filter Installation</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li class="nav__item has-dropdown bg-black">
+                        <a href="#" data-toggle="dropdown" class="dropdown-toggle nav__item-link color-white">Air Purification</a>
+                        <ul class="dropdown-menu bg-black">
+                            <li class="nav__item">
+                                <a href="humidifier-installation.php" class="nav__item-link color-white">Humidifier Installation</a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="hepa-filter-installation.php" class="nav__item-link color-white">HEPA Filter Installation</a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="air-purifier-installation.php" class="nav__item-link color-white">Air Purifier Installation</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li class="nav__item has-dropdown bg-black">
+                        <a href="#" data-toggle="dropdown" class="dropdown-toggle nav__item-link color-white">AC</a>
+                        <ul class="dropdown-menu bg-black">
+                            <li class="nav__item">
+                                <a href="ac-installation.php" class="nav__item-link color-white">AC Installation</a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="ac-maintenance.php" class="nav__item-link color-white">AC Maintenance</a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="ac-repair.php" class="nav__item-link color-white">AC Repair</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li class="nav__item">
+                        <a href="contact-us.php" class="nav__item-link color-white">Contact Us</a>
+                    </li>
+
+
+                </ul>
+                <button class="close-mobile-menu d-block d-lg-none"><i class="fas fa-times"></i></button>
+
+            </div>
+
+        </div>
+    </nav>
+
+</header>
